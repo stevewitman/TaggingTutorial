3299989a61cf7b22d1c0327355c7d8f758938db1
Steve Witman <stevewitman@gmail.com>
2014-11-22 02:04:57 -0700

    working tag tutorial

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141121195209) do
+ActiveRecord::Schema.define(version: 20141121204831) do
 
   create_table "posts", force: true do |t|
     t.string   "author"
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "taggings", force: true do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
   end
 
   create_table "tags", force: true do |t|
